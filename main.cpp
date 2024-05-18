441bb0a244fe5cffd2100a2991f0b2294c58c7cc
Nikita Krenov <nikitabaram@gmail.com>
2024-05-18 17:05:08 +0300

    min and max profit in array but we have profit for all year and also found max and min number in month for company

--- main.cpp
+++ main.cpp
@@ -2,12 +2,42 @@
 #include <ctime>
 using namespace std;
 
-
 int main() {
-	
+    const int months = 12;
+    double profit[months];
+
+    cout << "Enter the profit for each month: " << endl;
+    for (int i = 0; i < months; i++) {
+        cout << "Month " << i + 1 << ": ";
+        cin >> profit[i];
+    }
+
+    int firstNumberInDip, lastNumberInDip;
+
+    cout << "Enter the range of months to check for dips in profit (first and last month numbers): ";
+    cin >> firstNumberInDip >> lastNumberInDip;
+
+    if (firstNumberInDip < 1 || firstNumberInDip > months || lastNumberInDip < 1 || lastNumberInDip > months || firstNumberInDip > lastNumberInDip) {
+        cout << "Invalid range!" << endl;
+        return 1;
+    }
+
+    double maxProfit = profit[firstNumberInDip - 1];
+    double minProfit = profit[firstNumberInDip - 1];
+
+    cout << "Dips in profit: " << endl;
+    for (int i = firstNumberInDip - 1; i < lastNumberInDip; i++) {
+        if (profit[i] > maxProfit) {
+            maxProfit = profit[i];
+        }
+        if (profit[i] < minProfit) {
+            minProfit = profit[i];
+        }
+    }
+
+    cout << "Max profit: " << maxProfit << endl;
+    cout << "Min profit: " << minProfit << endl;
 
 	system("pause");
-	return 0;
+    return 0;
 }
-
-
