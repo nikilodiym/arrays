009c69501a0d3f9c8dfd29b7e2f44ee066b1a53e
Nikita Krenov <nikitabaram@gmail.com>
2024-05-18 16:03:18 +0300

    enter two range limits and a number from the keyboard. If the number does not fall within the range, the program prompts the user to re-enter the number, and so on until they enter the number correctly.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 
 int main() {
-	
+	double profitForTheMonth[6];
+	double allProfit = 0;
+
+	for (int i = 0; i < 6; i++) {
+		cout << "Enter the profit for the month " << i + 1 << ": ";
+		cin >> profitForTheMonth[i];
+		allProfit += profitForTheMonth[i];
+	}
+
+	cout << "The profit for the 6 months is: " << allProfit << endl;
 
 	system("pause");
 	return 0;
